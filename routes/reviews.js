90dd699c093859a12cbf90db0843b576db56cc8a
Sasa Radovanovic <salle90ar@gmail.com>
2019-12-16 12:46:42 +0100

    Get Single Review & Update Seeder

---
+++ routes/reviews.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { getReviews, getReview } = require('../controllers/reviews');
+
+const Review = require('../models/Review');
+
+const router = express.Router({ mergeParams: true });
+
+const { protect, authorize } = require('../middleware/auth');
+const advancedResults = require('../middleware/advancedResults');
+
+router.route('/').get(
+  advancedResults(Review, {
+    path: 'bootcamp',
+    select: 'name description'
+  }),
+  getReviews
+);
+
+router.route('/:id').get(getReview);
+
+module.exports = router;
